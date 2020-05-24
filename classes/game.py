d9ad056849ae8345bc7addbd3de634d522854d14
tworrell <tara.worrell@gmail.com>
2020-05-24 19:00:29 -0400

    Dealt with whitespace in progress bars when they're deleted.

--- classes/game.py
+++ classes/game.py
@@ -100,6 +100,32 @@ class Person:
         while len(mp_bar) < 10:
             mp_bar += " "
 
+        hp_string = str(self.hp) + "/" + str(self.maxhp)
+        current_hp = ""
+
+        if len(hp_string) < 9:
+            decreased = 9 - len(hp_string)
+
+            while decreased > 0:
+                current_hp += " "
+                decreased -= 1
+
+            current_hp += hp_string
+        else:
+            current_hp = hp_string
+
+        mp_string = str(self.mp) + "/" + str(self.maxmp)
+        current_mp = ""
+
+        if len(mp_string) < 7:
+            decreased = 7 - len(mp_string)
+            while decreased > 0:
+                current_mp += " "
+                decreased -= 1
+
+            current_mp += mp_string
+        else:
+            current_mp = mp_string
 
         print("                      _________________________              __________")
-        print(bcolors.BOLD + str(self.name) + "      " + str(self.hp) + "/" + str(self.maxhp) + "|" + bcolors.OKGREEN + hp_bar + bcolors.ENDC + bcolors.BOLD + "|     " + str(self.mp) + "/" + str(self.maxmp) + "|" + bcolors.OKBLUE + mp_bar + bcolors.ENDC + "|")
+        print(bcolors.BOLD + str(self.name) + "      " + current_hp + "|" + bcolors.OKGREEN + hp_bar + bcolors.ENDC + bcolors.BOLD + "|     " + current_mp  + "|" + bcolors.OKBLUE + mp_bar + bcolors.ENDC + "|")
